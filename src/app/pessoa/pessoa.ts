a00de8af782718d566d489d12b5011ccc407545c
Eduardo <dudufontes01.ef@gmail.comcd>
2026-08-25 22:33:32 -0300

    commit inicial do projeto angular

---
+++ src/app/pessoa/pessoa.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  imports: [],
+  selector: 'app-pessoa',
+  styleUrl: './pessoa.css',
+  templateUrl: './pessoa.html',
+})
+export class Pessoa {}
